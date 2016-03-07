cf4c4be406686ee79a3fed93f1ddd34ae4d2f1f1
1510215 <d.kannan@rgu.ac.uk>
2016-03-07 10:21:43 +0000

    php test 2

--- week6PHP/activity1/index.php
+++ week6PHP/activity1/index.php
@@ -4,9 +4,30 @@
 </head>
 <body>
 <p>
+
+
+
     <?php
     echo "Hello World";
     ?>
+
+    <?php
+    $myname = "dinesh";
+    $strangername = paul;
+
+        if($myname == "dinesh") {
+
+            echo "Hello Dinesh !";
+        }
+
+        else {
+                echo "Who are you ?";
+            }
+    ?>
+
+
+
+
 </p>
 </body>
 </html>
