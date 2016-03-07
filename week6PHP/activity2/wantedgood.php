f4740a99c92b92350ef01ce181eb6fcc383f87e1
1510215 <d.kannan@rgu.ac.uk>
2016-03-07 11:40:25 +0000

    activity 2 ---> 3

--- week6PHP/activity2/wantedgood.php
+++ week6PHP/activity2/wantedgood.php
@@ -14,7 +14,7 @@
 
 
     <form action="" method="post">
-         Enter object name: <input type="text" name="userinput" value="Enter oject name"><br>
+         Enter object name: <input type="text" name="userinput" value="Enter object name"><br>
          <input type="submit">
     </form>
 
