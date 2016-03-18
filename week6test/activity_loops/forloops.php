6b5bb27de86e7f13a4688b3f74ef163abdc2ccb8
kannandineshan <d.kannan@rgu.ac.uk>
2016-03-18 03:28:07 +0000

    a

--- week6test/activity_loops/forloops.php
+++ week6test/activity_loops/forloops.php
@@ -7,9 +7,14 @@
 <?php
 
 
-for($i=1;$i<30;$i*=2){
+for($i=1;$i<30;$i++){
 
-    echo "<p>hello</p>";
+    for($i=1;$i<30;$i*=2){ echo "<p>On the 2nd of the month specs are available</p>";}
+
+    for($i=1;$i<30;$i*=3){ echo "<p>On the 3rd of the month mugs are available</p>";}
+
+    for($i=1;$i<30;$i*=4){ echo "<p>On the 4th of the month specs and sausage rolls are available</p>";}
+
 }
 
 
