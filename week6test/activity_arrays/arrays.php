b60dbf861d26aa3c5c7477879c4b19b65c1f297a
kannandineshan <d.kannan@rgu.ac.uk>
2016-03-18 01:36:45 +0000

    a

--- week6test/activity_arrays/arrays.php
+++ week6test/activity_arrays/arrays.php
@@ -12,14 +12,19 @@
 $provisionedActivities = array("Specs","Mugs","Sausage rolls");
 
 
+foreach($provisionedActivities as $x){
+
+    print "<p>$x</p>";
+}
+
 $provisionedActivities[1]= "hugs";
 unset($provisionedActivities[2]);
-
 
 foreach($provisionedActivities as $x){
 
     print "<p>$x</p>";
 }
+
 
 ?>
 
