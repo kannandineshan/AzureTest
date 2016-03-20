b96345892c802fe85dbdc9e3c3df46046488dfa7
kannandineshan <d.kannan@rgu.ac.uk>
2016-03-20 23:34:24 +0000

    week7

--- week7database/index.php
+++ week7database/index.php
@@ -18,7 +18,7 @@
 <label>Password:</label><br>
 <input type="password" name="password" placeholder="password"/>
 <br><br>
-<input type="button" name="submit" value ="login" formmethod="post" formaction="login.php"/>
+<button formmethod="post" formaction="login.php"></button>
 
 <div class="error"><?php //echo $error;?><?php //echo $username; echo $password;?></div>
 </div>
