69dde3a27e398128eb466bade0ff127b6db28443
kannandineshan <d.kannan@rgu.ac.uk>
2016-03-20 22:08:59 +0000

    week7

---
+++ week5/fucntion.js
@@ -0,0 +1,12 @@
+
+$( "#cont11" ).click(function() {
+    $( "#cont1" ).toggle( "bounce", 1000 );
+});
+
+$( "#cont22" ).click(function() {
+    $( "#cont2" ).toggle( "size", 1000 );
+});
+
+$( "#cont33" ).click(function() {
+    $( "#cont3" ).toggle( "fold", 1000 );
+});
