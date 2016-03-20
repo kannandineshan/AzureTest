6f3b20ddccc889cd8feebd7e00407501c20a8393
kannandineshan <d.kannan@rgu.ac.uk>
2016-03-20 23:25:57 +0000

    week7

--- week7database/connection.php
+++ week7database/connection.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_SERVER', 'your-azure-server');
-define('DB_USERNAME', 'your-username');
-define('DB_PASSWORD', 'your-password');
-define('DB_DATABASE', 'your-database');
+define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
+define('DB_USERNAME', 'b7769b62608ed3');
+define('DB_PASSWORD', 'dd907097');
+define('DB_DATABASE', 'kannandineshan');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
 
