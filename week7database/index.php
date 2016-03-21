e5d22a7411e576dc337d797aa5f1ac5aa296b177
1510215 <d.kannan@rgu.ac.uk>
2016-03-21 09:24:26 +0000

    activity 2 ---> 3

--- week7database/index.php
+++ week7database/index.php
@@ -1,46 +1,26 @@
 <!DOCTYPE html>
+
 <html>
 <head>
     <meta charset="utf-8">
     <title>PHP Login Form without Session</title>
-    <link rel="stylesheet" href="style.css" type="text/css"/>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-    <script>
-        $(document).ready(function(){
-            $("button").click(function(){
-                $.post("demo_test_post.asp",
-                    {
-                        name: "#name",
-                        password: "#password"
-
-                    },
-                    function(data,status){
-                        alert("Data: " + data + "\nStatus: " + status);
-                    });
-            });
-        });
-    </script>
+    <link rel="stylesheet" href="style.css" type="text/css" />
 </head>
 <body>
-<h1>PHP Login Form without Session</h1>
+<h1>PHP Login Form with Session</h1>
 <div class="loginBox">
-<h3>Login Form</h3>
-<br><br>
-
-<label>Username:</label>
-<br>
-<input id="name" type="text" name="username" placeholder="username"/>
-<br><br>
-<label>Password:</label><br>
-<input  id="password" type="password" name="password" placeholder="password"/>
-<br><br>
-
-    <button>Send an HTTP POST request to a page and get the result back</button>
-
-
-<div class="error"><?php //echo $error;?><?php //echo $username; echo $password;?></div>
+    <h3>Login Form</h3>
+    <br><br>
+    <form method="post" action="login.php">
+        <label>Username:</label><br>
+        <input type="text" name="username" placeholder="username"
+            /><br><br>
+        <label>Password:</label><br>
+        <input type="password" name="password" placeholder="password" />
+        <br><br>
+        <input type="submit" name="submit" value = "login"/>
+    </form>
+    <div class="error"><?php //echo $error;?><?php //echo $username; echo $password;?></div>
 </div>
-
-
 </body>
 </html>
