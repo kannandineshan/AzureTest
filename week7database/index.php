1b09bd2eaaf29a58d4d311bf5bc44086e24b8d30
1510215 <d.kannan@rgu.ac.uk>
2016-03-22 09:20:53 +0000

    0

--- week7database/index.php
+++ week7database/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <title>PHP Login Form without Session</title>
-    <link rel="stylesheet" href="style.css" type="text/css" />
+
 </head>
 <body>
 <h1>PHP Login Form with Session</h1>
