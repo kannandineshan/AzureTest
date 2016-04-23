f02c100f45f43c3ced4f5b7ec7d5838fa4665fa9
kannandineshan <d.kannan@rgu.ac.uk>
2016-04-23 16:51:00 +0100

    0

--- Dinesh/volunteerhome.php
+++ Dinesh/volunteerhome.php
@@ -93,7 +93,8 @@ function get_volunteer_name($email){
 <head>
     <!-- - CHARACTER ENCODING - -->
     <meta charset="UTF-8">
-
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- - WINDOW TAB TITLE - -->
     <title>Volunteer Homepage</title>
 
@@ -102,15 +103,22 @@ function get_volunteer_name($email){
 
     <!-- - CSS Stylesheet- -->
     <link rel="stylesheet" href="volunteerhome_assets/volunteerhome_css/volunteerhome.css" type="text/css">
+    <link rel="stylesheet" type="text/css" href="volunteerhome_assets/volunteerhome_css/slicebox.css" />
 
     <!-- - JQUERY SCRIPT- -->
     <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
     <script src="//code.jquery.com/jquery-1.10.2.js"></script>
     <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+
+
+
+
+
+
 
 
 
@@ -233,126 +241,73 @@ function get_volunteer_name($email){
     </main>
     <!-- (END OF MAIN) - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
 
-    <footer>
-        <div class="container">
 
-            <div class="wrapper">
+    <div class="container">
+        <div class="wrapper">
 
-                <ul id="sb-slider" class="sb-slider">
-                    <li>
-                        <a href="http://www.flickr.com/photos/strupler/2969141180" target="_blank"><img src="images/1.jpg" alt="image1"/></a>
-                        <div class="sb-description">
-                            <h3>Creative Lifesaver</h3>
-                        </div>
-                    </li>
-                    <li>
-                        <a href="http://www.flickr.com/photos/strupler/2968268187" target="_blank"><img src="images/2.jpg" alt="image2"/></a>
-                        <div class="sb-description">
-                            <h3>Honest Entertainer</h3>
-                        </div>
-                    </li>
-                    <li>
-                        <a href="http://www.flickr.com/photos/strupler/2968114825" target="_blank"><img src="images/3.jpg" alt="image1"/></a>
-                        <div class="sb-description">
-                            <h3>Brave Astronaut</h3>
-                        </div>
-                    </li>
-                    <li>
-                        <a href="http://www.flickr.com/photos/strupler/2968122059" target="_blank"><img src="images/4.jpg" alt="image1"/></a>
-                        <div class="sb-description">
-                            <h3>Affectionate Decision Maker</h3>
-                        </div>
-                    </li>
-                    <li>
-                        <a href="http://www.flickr.com/photos/strupler/2969119944" target="_blank"><img src="images/5.jpg" alt="image1"/></a>
-                        <div class="sb-description">
-                            <h3>Faithful Investor</h3>
-                        </div>
-                    </li>
-                    <li>
-                        <a href="http://www.flickr.com/photos/strupler/2968126177" target="_blank"><img src="images/6.jpg" alt="image1"/></a>
-                        <div class="sb-description">
-                            <h3>Groundbreaking Artist</h3>
-                        </div>
-                    </li>
-                    <li>
-                        <a href="http://www.flickr.com/photos/strupler/2968945158" target="_blank"><img src="images/7.jpg" alt="image1"/></a>
-                        <div class="sb-description">
-                            <h3>Selfless Philantropist</h3>
-                        </div>
-                    </li>
-                </ul>
+            <ul id="sb-slider" class="sb-slider">
+                <li>
+                    <img src="volunteerhome_assets/volunteerhome_images/1.jpg" alt="image1"/>
+                    <div class="sb-description">
+                        <textarea required name="question1" cols="45" rows="5" placeholder="Enter your response here" id="question1"></textarea>
+                    </div>
+                </li>
+                <li>
+                    <a href="http://www.flickr.com/photos/strupler/2968268187" target="_blank"><img src="volunteerhome_assets/volunteerhome_images/2.jpg" alt="image2"/></a>
+                    <div class="sb-description">
+                        <h3>Honest Entertainer</h3>
+                    </div>
+                </li>
+                <li>
+                    <a href="http://www.flickr.com/photos/strupler/2968114825" target="_blank"><img src="volunteerhome_assets/volunteerhome_images/3.jpg" alt="image1"/></a>
+                    <div class="sb-description">
+                        <h3>Brave Astronaut</h3>
+                    </div>
+                </li>
+                <li>
+                    <a href="http://www.flickr.com/photos/strupler/2968122059" target="_blank"><img src="volunteerhome_assets/volunteerhome_images/4.jpg" alt="image1"/></a>
+                    <div class="sb-description">
+                        <h3>Affectionate Decision Maker</h3>
+                    </div>
+                </li>
+                <li>
+                    <a href="http://www.flickr.com/photos/strupler/2969119944" target="_blank"><img src="volunteerhome_assets/volunteerhome_images/5.jpg" alt="image1"/></a>
+                    <div class="sb-description">
+                        <h3>Faithful Investor</h3>
+                    </div>
+                </li>
+                <li>
+                    <a href="http://www.flickr.com/photos/strupler/2968126177" target="_blank"><img src="volunteerhome_assets/volunteerhome_images/6.jpg" alt="image1"/></a>
+                    <div class="sb-description">
+                        <h3>Groundbreaking Artist</h3>
+                    </div>
+                </li>
+                <li>
+                    <a href="http://www.flickr.com/photos/strupler/2968945158" target="_blank"><img src="volunteerhome_assets/volunteerhome_images/7.jpg" alt="image1"/></a>
+                    <div class="sb-description">
+                        <h3>Selfless Philantropist</h3>
+                    </div>
+                </li>
+            </ul>
 
-                <div id="shadow" class="shadow"></div>
+            <div id="shadow" class="shadow"></div>
 
-                <div id="nav-arrows" class="nav-arrows">
-                    <a href="#">Next</a>
-                    <a href="#">Previous</a>
-                </div>
+            <div id="nav-arrows" class="nav-arrows">
+                <a href="#">Next</a>
+                <a href="#">Previous</a>
+            </div>
 
-            </div><!-- /wrapper -->
-
-
-        </div>
+        </div><!-- /wrapper -->
 
 
-        <script type="text/javascript">
-            $(function() {
-
-                var Page = (function() {
-
-                    var $navArrows = $( '#nav-arrows' ).hide(),
-                        $shadow = $( '#shadow' ).hide(),
-                        slicebox = $( '#sb-slider' ).slicebox( {
-                            onReady : function() {
-
-                                $navArrows.show();
-                                $shadow.show();
-
-                            },
-                            orientation : 'r',
-                            cuboidsRandom : true,
-                            disperseFactor : 30
-                        } ),
-
-                        init = function() {
-
-                            initEvents();
-
-                        },
-                        initEvents = function() {
-
-                            // add navigation events
-                            $navArrows.children( ':first' ).on( 'click', function() {
-
-                                slicebox.next();
-                                return false;
-
-                            } );
-
-                            $navArrows.children( ':last' ).on( 'click', function() {
-
-                                slicebox.previous();
-                                return false;
-
-                            } );
-
-                        };
-
-                    return { init : init };
-
-                })();
-
-                Page.init();
-
-            });
-        </script>
+    </div>
 
 
-    </footer>
     <!-- CALL JQUERY SCRIPT FUNCTIONS -->
     <script SRC="volunteerhome_assets/volunteerhome_jquery/surveybounce.js"></script>
+    <script type="text/javascript" src="volunteerhome_assets/volunteerhome_jquery/jquery.slicebox.js"></script>
     <script SRC="volunteerhome_assets/volunteerhome_jquery/modernizr-custom.js"></script>
+    <script type="text/javascript" src="volunteerhome_assets/volunteerhome_jquery/modernizr.custom.46884.js"></script>
 
 </body>
 <!-- [END OF BODY] ================================================================================================= -->
