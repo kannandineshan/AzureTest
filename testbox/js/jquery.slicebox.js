71fb5150923b55a61692dd91ae39d4b10ec210f5
kannandineshan <d.kannan@rgu.ac.uk>
2016-04-23 17:40:56 +0100

    0

--- testbox/js/jquery.slicebox.js
+++ testbox/js/jquery.slicebox.js
@@ -1,3 +1,56 @@
+$(function() {
+
+	var Page = (function() {
+
+		var $navArrows = $( '#nav-arrows' ).hide(),
+			$shadow = $( '#shadow' ).hide(),
+			slicebox = $( '#sb-slider' ).slicebox( {
+				onReady : function() {
+
+					$navArrows.show();
+					$shadow.show();
+
+				},
+				orientation : 'r',
+				cuboidsRandom : true,
+				disperseFactor : 30
+			} ),
+
+			init = function() {
+
+				initEvents();
+
+			},
+			initEvents = function() {
+
+				// add navigation events
+				$navArrows.children( ':first' ).on( 'click', function() {
+
+					slicebox.next();
+					return false;
+
+				} );
+
+				$navArrows.children( ':last' ).on( 'click', function() {
+
+					slicebox.previous();
+					return false;
+
+				} );
+
+			};
+
+		return { init : init };
+
+	})();
+
+	Page.init();
+
+});
+
+
+
+
 /**
  * jquery.slicebox.js v1.1.0
  * http://www.codrops.com
@@ -910,52 +963,3 @@
 	
 } )( jQuery, window );
 
-$(function() {
-
-	var Page = (function() {
-
-		var $navArrows = $( '#nav-arrows' ).hide(),
-			$shadow = $( '#shadow' ).hide(),
-			slicebox = $( '#sb-slider' ).slicebox( {
-				onReady : function() {
-
-					$navArrows.show();
-					$shadow.show();
-
-				},
-				orientation : 'r',
-				cuboidsRandom : true,
-				disperseFactor : 30
-			} ),
-
-			init = function() {
-
-				initEvents();
-
-			},
-			initEvents = function() {
-
-				// add navigation events
-				$navArrows.children( ':first' ).on( 'click', function() {
-
-					slicebox.next();
-					return false;
-
-				} );
-
-				$navArrows.children( ':last' ).on( 'click', function() {
-
-					slicebox.previous();
-					return false;
-
-				} );
-
-			};
-
-		return { init : init };
-
-	})();
-
-	Page.init();
-
-});
