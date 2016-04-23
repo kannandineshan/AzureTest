dd6a5bb8a6a935eb321a6e6f4c59da8c0467fa97
kannandineshan <d.kannan@rgu.ac.uk>
2016-04-23 17:41:42 +0100

    0

--- Dinesh/volunteerhome.php
+++ Dinesh/volunteerhome.php
@@ -235,65 +235,6 @@ function get_volunteer_name($email){
         </form>
         <!-- (END OF SURVEY) - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  -->
 
-        <div class="container">
-            <div class="wrapper">
-
-                <ul id="sb-slider" class="sb-slider">
-                    <li>
-                        <img src="volunteerhome_assets/volunteerhome_images/1.jpg" alt="image1"/>
-                        <div class="sb-description">
-                            <textarea required name="question1" cols="45" rows="5" placeholder="Enter your response here" id="question1"></textarea>
-                        </div>
-                    </li>
-                    <li>
-                        <a href="http://www.flickr.com/photos/strupler/2968268187" target="_blank"><img src="volunteerhome_assets/volunteerhome_images/2.jpg" alt="image2"/></a>
-                        <div class="sb-description">
-                            <h3>Honest Entertainer</h3>
-                        </div>
-                    </li>
-                    <li>
-                        <a href="http://www.flickr.com/photos/strupler/2968114825" target="_blank"><img src="volunteerhome_assets/volunteerhome_images/3.jpg" alt="image1"/></a>
-                        <div class="sb-description">
-                            <h3>Brave Astronaut</h3>
-                        </div>
-                    </li>
-                    <li>
-                        <a href="http://www.flickr.com/photos/strupler/2968122059" target="_blank"><img src="volunteerhome_assets/volunteerhome_images/4.jpg" alt="image1"/></a>
-                        <div class="sb-description">
-                            <h3>Affectionate Decision Maker</h3>
-                        </div>
-                    </li>
-                    <li>
-                        <a href="http://www.flickr.com/photos/strupler/2969119944" target="_blank"><img src="volunteerhome_assets/volunteerhome_images/5.jpg" alt="image1"/></a>
-                        <div class="sb-description">
-                            <h3>Faithful Investor</h3>
-                        </div>
-                    </li>
-                    <li>
-                        <a href="http://www.flickr.com/photos/strupler/2968126177" target="_blank"><img src="volunteerhome_assets/volunteerhome_images/6.jpg" alt="image1"/></a>
-                        <div class="sb-description">
-                            <h3>Groundbreaking Artist</h3>
-                        </div>
-                    </li>
-                    <li>
-                        <a href="http://www.flickr.com/photos/strupler/2968945158" target="_blank"><img src="volunteerhome_assets/volunteerhome_images/7.jpg" alt="image1"/></a>
-                        <div class="sb-description">
-                            <h3>Selfless Philantropist</h3>
-                        </div>
-                    </li>
-                </ul>
-
-                <div id="shadow" class="shadow"></div>
-
-                <div id="nav-arrows" class="nav-arrows">
-                    <a href="#">Next</a>
-                    <a href="#">Previous</a>
-                </div>
-
-            </div><!-- /wrapper -->
-
-
-        </div>
 
     </main>
     <!-- (END OF MAIN) - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
