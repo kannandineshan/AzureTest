3bf7c1652b92513b7006ecfddcbbf1c643008a68
kannandineshan <d.kannan@rgu.ac.uk>
2016-04-30 20:48:23 +0100

    0

--- mocklab/task3/showbugs.php
+++ mocklab/task3/showbugs.php
@@ -76,7 +76,7 @@
                 arbitrary operations on the victim device through remote code execution and privilege escalation.
             </div>
 
-            <section class="container-content" id="line-space"></div>
+            <section class="container-line-space" id="line-space"></div>
 
             <div class="title"> Bug Name: Dr Crabbs Evil Bugt</div>
             <br>
