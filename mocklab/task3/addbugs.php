2f17d24dec2bd11b126bc9ed21ee5c242b16538f
kannandineshan <d.kannan@rgu.ac.uk>
2016-05-01 20:57:09 +0100

    0

--- mocklab/task3/addbugs.php
+++ mocklab/task3/addbugs.php
@@ -53,10 +53,10 @@
 
             <nav>
                 <ul>
-                    <li><a href="showbugs.php">All Bug Items</a> </li>
-                    <li><a href="showbugs.php">Android Bugs</a> </li>
-                    <li><a href="showbugs.php">iOS Bugs</a> </li>
-                    <li><a href="showbugs.php">Windows Bugs</a> </li>
+                    <li><a href="showBugs.php?bugCategory=*">All Bug Items</a> </li>
+                    <li><a href="showBugs.php?bugCategory=Android">Android Bugs</a> </li>
+                    <li><a href="showBugs.php?bugCategory=iOS">iOS Bugs</a> </li>
+                    <li><a href="showBugs.php?bugCategory=Windows">Windows Bugs</a> </li>
                     <li><a href="addbugs.php">Insert Bug</a> </li>
                 </ul>
 
