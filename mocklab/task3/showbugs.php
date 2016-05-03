8edbc3bc83198eeca7d98750cbca09d2304936fd
kannandineshan <d.kannan@rgu.ac.uk>
2016-05-03 07:00:00 +0100

    0

--- mocklab/task3/showbugs.php
+++ mocklab/task3/showbugs.php
@@ -73,16 +73,16 @@ include("assets/PHP/PHPfunctions.php");
                             $counter++;
                 ?>
                             <section class="bugitem">
-                                <section class="title"> Bug Number:</section><?php echo $counter; ?> </section>
+                                <section class="title"> Bug Number:</section><?php echo $counter; ?>
 
 
-                                <section class="title"> Bug Name:</section> <?php echo $row['bugName']; ?> </section>
+                                <section class="title"> Bug Name:</section> <?php echo $row['bugName']; ?>
 
 
-                                <section class="title"> Bug Category:</section> <?php echo $row['bugCategory']; ?> </section>
+                                <section class="title"> Bug Category:</section> <?php echo $row['bugCategory']; ?>
 
 
-                                <section class="title">Bug Summary:</section> <?php echo $row['bugSummary']; ?> </section>
+                                <section class="title">Bug Summary:</section> <?php echo $row['bugSummary']; ?>
                              </section>
                 <?php
 
